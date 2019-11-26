13bd4c1cf7b873ff1d7ea85ea8a7ac18c0d6a73f
CoderJobo <joan.hickey@ucdconnect.ie>
2019-11-26 20:13:25 +0000

    Add files via upload

---
+++ Chp2_Fundamental_Data_Types/P2.14 Printing a grid.cpp
@@ -0,0 +1,34 @@
+/*
+Write a program that prints the following grid to play tic-tac-toe
+
++--+--+--+
+|  |  |  |
++--+--+--+
+|  |  |  |
++--+--+--+
+|  |  |  |
++--+--+--+
+
+Of course, you could simply write seven statements of the form
+
+    cout << "+--+--+--+";
+
+You should do it the smart way, though.  Define string variables
+to hold two kinds of patterns: a comb-shaped pattern and the bottom
+line.  Print the comb three times and the bottom line once.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string comb = "+--+--+--+\n|  |  |  |\n";
+    string bottom = "+--+--+--+";
+
+    cout << comb << comb << comb;
+    cout << bottom;
+
+    return 0;
+}
